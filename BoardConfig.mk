92870e0c0612bc6d51216de264ad37c1787c279a
S4turn <91470154+Hyylassx@users.noreply.github.com>
2026-06-02 21:21:31 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,6 @@ DEVICE_PATH := device/infinix/X655C
 ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
-# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
