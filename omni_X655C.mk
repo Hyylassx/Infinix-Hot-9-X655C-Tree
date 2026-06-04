56b58039da91419ebed8c5ebff74528ce6697739
S4turn <91470154+Hyylassx@users.noreply.github.com>
2026-06-04 13:23:11 +0700

    Update omni_X655C.mk

--- omni_X655C.mk
+++ omni_X655C.mk
@@ -77,8 +77,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/recovery/root/system/lib64/libkeymaster_portable.so:recovery/root/system/lib64/libkeymaster_portable.so \
     $(DEVICE_PATH)/recovery/root/system/lib/libsoftkeymasterdevice.so:recovery/root/system/lib/libsoftkeymasterdevice.so \
     $(DEVICE_PATH)/recovery/root/system/lib64/libsoftkeymasterdevice.so:recovery/root/system/lib64/libsoftkeymasterdevice.so \
-    $(DEVICE_PATH)/recovery/root/system/lib/android.hidl.allocator@1.0.so:recovery/root/system/lib/android.hidl.allocator@1.0.so \
-    $(DEVICE_PATH)/recovery/root/system/lib64/android.hidl.allocator@1.0.so:recovery/root/system/lib64/android.hidl.allocator@1.0.so \
     $(DEVICE_PATH)/recovery/root/system/lib/libhidlmemory.so:recovery/root/system/lib/libhidlmemory.so \
     $(DEVICE_PATH)/recovery/root/system/lib64/libhidlmemory.so:recovery/root/system/lib64/libhidlmemory.so \
     $(DEVICE_PATH)/recovery/root/system/lib/libteeservice_client.trustonic.so:recovery/root/system/lib/libteeservice_client.trustonic.so \
