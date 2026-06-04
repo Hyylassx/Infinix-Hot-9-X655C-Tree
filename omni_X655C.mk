86e49bc60dbf3dc602322e9ad42fde4afcf93a14
S4turn <91470154+Hyylassx@users.noreply.github.com>
2026-06-04 12:51:17 +0700

    Update omni_X655C.mk

--- omni_X655C.mk
+++ omni_X655C.mk
@@ -79,8 +79,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/recovery/root/system/lib64/libkeymaster_portable.so:recovery/root/system/lib64/libkeymaster_portable.so \
     $(DEVICE_PATH)/recovery/root/system/lib/libsoftkeymasterdevice.so:recovery/root/system/lib/libsoftkeymasterdevice.so \
     $(DEVICE_PATH)/recovery/root/system/lib64/libsoftkeymasterdevice.so:recovery/root/system/lib64/libsoftkeymasterdevice.so \
-    $(DEVICE_PATH)/recovery/root/system/lib/android.hidl.memory@1.0.so:recovery/root/system/lib/android.hidl.memory@1.0.so \
-    $(DEVICE_PATH)/recovery/root/system/lib64/android.hidl.memory@1.0.so:recovery/root/system/lib64/android.hidl.memory@1.0.so \
     $(DEVICE_PATH)/recovery/root/system/lib/android.hardware.boot@1.0.so:recovery/root/system/lib/android.hardware.boot@1.0.so \
     $(DEVICE_PATH)/recovery/root/system/lib64/android.hardware.boot@1.0.so:recovery/root/system/lib64/android.hardware.boot@1.0.so \
     $(DEVICE_PATH)/recovery/root/system/lib/android.hidl.allocator@1.0.so:recovery/root/system/lib/android.hidl.allocator@1.0.so \
