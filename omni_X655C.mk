a39e6004ba50621371a9c572cd3192fab3935981
S4turn <91470154+Hyylassx@users.noreply.github.com>
2026-06-05 13:11:30 +0700

    Update omni_X655C.mk

--- omni_X655C.mk
+++ omni_X655C.mk
@@ -80,7 +80,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/recovery/root/vendor/lib64/libcrypto-mdapp.so:vendor/lib64/libcrypto-mdapp.so \
     $(DEVICE_PATH)/recovery/root/vendor/lib64/libimsg_log.so:vendor/lib64/libimsg_log.so \
     $(DEVICE_PATH)/recovery/root/vendor/lib64/libion_mtk.so:vendor/lib64/libion_mtk.so \
-    $(DEVICE_PATH)/recovery/root/vendor/lib64/libkeymaster4.so:vendor/lib64/libkeymaster4.so \
     $(DEVICE_PATH)/recovery/root/vendor/lib64/libmtee.so:vendor/lib64/libmtee.so \
     $(DEVICE_PATH)/recovery/root/vendor/lib64/libTEECommon.so:vendor/lib64/libTEECommon.so \
     $(DEVICE_PATH)/recovery/root/vendor/lib64/vendor.microtrust.hardware.capi@2.0.so:vendor/lib64/vendor.microtrust.hardware.capi@2.0.so \
